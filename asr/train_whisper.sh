5e89c84ed4eed5e7bb10702861a839b9ce8315b9
Hexin Liu <32285422+Lhx94As@users.noreply.github.com>
2026-03-01 11:15:09 +0800

    Delete asr directory

--- asr/train_whisper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cmd="/home3/chenchen/research/maison2/egs/VB/slurm.pl --quiet --nodelist=node05 --gpu 1"
-
-# source activate whisper
-
-
-$cmd log/train_whisper_asru.log \
-python train_whisper.py --save_every 10000 --lr 0.00001
